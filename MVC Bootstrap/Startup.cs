8462d253b6eecb44fa6436ffa5effc67bdeb4b00
Javed Asghar <javaidchaudary@gmail.com>
2016-06-12 11:23:04 +0500

    Add files via upload

---
+++ MVC Bootstrap/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_Bootstrap.Startup))]
+namespace MVC_Bootstrap
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
