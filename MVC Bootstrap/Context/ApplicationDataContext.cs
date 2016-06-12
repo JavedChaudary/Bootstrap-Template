fbcd06122cbb71842d2facbec5ecfcdac47e4178
Javed Asghar <javaidchaudary@gmail.com>
2016-06-12 11:29:03 +0500

    Add files via upload

---
+++ MVC Bootstrap/Context/ApplicationDataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MVC_Bootstrap.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace MVC_Bootstrap.Context
+{
+    public class ApplicationDataContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDataContext()
+            : base("DefaultConnection")
+        { }
+
+        public System.Data.Entity.DbSet<AppUser> AppUsers { get; set; }
+    }
+}
---
+++ MVC Bootstrap/Models/IdentityModels.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace MVC_Bootstrap.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
